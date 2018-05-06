cd122fd16495d3c90d653e9c4c5b09f2e63ca744
Jithin Abraham <jithin.abraham@cybrilla.com>
2018-05-06 22:43:28 +0530

    Dashboard | show api key in the tenant list

--- app/facades/dashboard.rb
+++ app/facades/dashboard.rb
@@ -17,7 +17,8 @@ class Dashboard
   end
 
   def tenants_with_request
-    @tenants_with_request ||= Tenant.joins('left outer join request_trackers on request_trackers.tenant_id = tenants.id')
+    @tenants_with_request ||= Tenant.joins('left outer join request_trackers on
+                                           request_trackers.tenant_id = tenants.id')
                               .select('id', 'tenants.name',
                                       'sum(request_trackers.request_count) as requests_count',
                                       'tenants.api_key')
